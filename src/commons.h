7d34429800b40c8fa0fbad85d65343ec1c2b2176
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2020-06-11 02:37:58 -0300

    se agrego pll.c pll.h y se agregaron TDA a commons.h

--- src/commons.h
+++ src/commons.h
@@ -2,8 +2,20 @@
 #define COMMONS_H
 
 typedef unsigned int uint32_t;
-typedef unsigned int uint16_t;
-typedef unsigned int uint8_t;
+typedef short unsigned int uint16_t;
+typedef unsigned char uint8_t;
+
+typedef int int32_t;
+typedef short int int16_t;
+typedef char int8_t;
+
+#define     __R					volatile const  	// !< Modificador para solo lectura
+#define 	__W     			volatile 	       	// !<  Modificador para solo escritura
+#define 	__RW				volatile           	// !< Modificador lectura / escritura
+#define 	ON					1
+#define 	OFF					0
+
+#define 	NULL				(( void *) 0)
 
 #define SALIDA      1
 #define ENTRADA     0
@@ -22,10 +34,11 @@ typedef enum{
     PIN8, PIN9, PIN10, PIN11, PIN12, PIN13, PIN14, PIN15,
     PIN16, PIN17, PIN18, PIN19, PIN20, PIN21, PIN22, PIN23,
     PIN24, PIN25, PIN26, PIN27, PIN28, PIN29, PIN30, PIN31,
-}pin_t;
+} pin_t;
 
 typedef enum{
     False,
     True
 } bool_t;
+
 #endif  // COMMONS_H
